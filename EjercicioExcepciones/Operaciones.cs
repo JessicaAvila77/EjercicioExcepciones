ee3fa5113818b8cc4f8aa3ce501cc9cf72abc185
JessicaAvila77 <jessica.avilaunah@gmail.com>
2024-08-09 21:10:13 -0600

    Agregando clase Operaciones

---
+++ EjercicioExcepciones/Operaciones.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioExcepciones
+{
+    public class Operaciones
+    {
+        public double N1 { get; set; }
+        public double N2 { get; set; }
+
+        public Operaciones() { }
+
+        public double Suma()
+        {
+            return N1 + N2;
+        }
+
+        public double Resta()
+        {
+            return N1 - N2;
+        }
+
+        public double Multiplicacion()
+        {
+            return N1 * N2;
+        }
+
+        public double Division()
+        {         
+                return N1 / N2;                     
+        }
+
+
+
+
+
+    }
+}
--- EjercicioExcepciones/Program.cs
+++ EjercicioExcepciones/Program.cs
@@ -6,10 +6,12 @@ using System.Threading.Tasks;
 
 namespace EjercicioExcepciones
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+
+
         }
     }
 }
